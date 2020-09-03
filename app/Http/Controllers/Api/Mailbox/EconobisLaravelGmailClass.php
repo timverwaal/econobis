a5f4bfd6e8362a17d5d59fba46a32de2435383f6
Wim <wim@mosmania.nl>
2020-09-03 14:07:53 +0200

    EconobisLaravelGmailClass with extends LaravelGmailClass instead of copy of LaravelGmailClass

--- app/Http/Controllers/Api/Mailbox/EconobisLaravelGmailClass.php
+++ app/Http/Controllers/Api/Mailbox/EconobisLaravelGmailClass.php
@@ -4,69 +4,15 @@ namespace App\Http\Controllers\Api\Mailbox;
 
 use Dacastro4\LaravelGmail\Exceptions\AuthException;
 use Dacastro4\LaravelGmail\GmailConnection;
+use Dacastro4\LaravelGmail\LaravelGmailClass;
 use Dacastro4\LaravelGmail\Services\Message;
 use Illuminate\Support\Facades\Redirect;
 
-class EconobisLaravelGmailClass extends GmailConnection
+abstract class EconobisLaravelGmailClass extends LaravelGmailClass
 {
 	public function __construct($config, $userId = null)
 	{
 		parent::__construct($config, $userId);
-	}
-
-	/**
-	 * @return Message
-	 * @throws AuthException
-	 */
-	public function message()
-	{
-		if (!$this->getToken()) {
-			throw new AuthException('No credentials found.');
-		}
-
-		return new Message($this);
-	}
-
-	/**
-	 * Returns the Gmail user email
-	 *
-	 * @return \Google_Service_Gmail_Profile
-	 */
-	public function user()
-	{
-		return $this->config('email');
-	}
-
-	/**
-	 * Updates / sets the current userId for the service
-	 *
-	 * @return \Google_Service_Gmail_Profile
-	 */
-	public function setUserId($userId)
-	{
-		$this->userId = $userId;
-		return $this;
-	}
-
-	public function redirect()
-	{
-		return Redirect::to($this->getAuthUrl());
-	}
-
-	/**
-	 * Gets the URL to authorize the user
-	 *
-	 * @return string
-	 */
-	public function getAuthUrl()
-	{
-		return $this->createAuthUrl();
-	}
-
-	public function logout()
-	{
-		$this->revokeToken();
-		$this->deleteAccessToken();
 	}
 
 }
